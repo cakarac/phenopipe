53f9dd57b56a974d04597aab5b3f53a3c62644cd
cakarac <adnancihan.cakar@vumc.org>
2025-07-02 17:33:31 +0000

    makes task init arguments truly optional

--- src/phenopipe/tasks/task.py
+++ src/phenopipe/tasks/task.py
@@ -9,13 +9,13 @@ class Task(BaseModel):
     '''Generic task class representing one step in analysis.'''
     
     #: input dataframes
-    inputs: dict[str, PolarsDataFrame] | None
+    inputs: Optional[dict[str, PolarsDataFrame]] = None
 
     #: environment variables applied to each task in analysis plan
-    env_vars: dict[str, str] | None
+    env_vars: Optional[dict[str, str]] = None
 
     #: output of complete method representing result of task 
-    output: PolarsDataFrame | None
+    output: Optional[PolarsDataFrame] = None
         
     def complete():
         pass
