e4f40428a4d2fd757e6b901dd6926da43e5534e6
cakarac <adnancihan.cakar@vumc.org>
2025-08-14 17:48:14 +0000

    adds all hospitalizations

--- src/phenopipe/tasks/get_data/hospitalization/__init__.py
+++ src/phenopipe/tasks/get_data/hospitalization/__init__.py
@@ -2,10 +2,12 @@ from .first_hf_hospitalization_data import FirstHfHospitalizationData
 from .first_copd_hospitalization_data import FirstCopdHospitalizationData
 from .first_mi_hospitalization_data import FirstMiHospitalizationData
 from .first_pneumonia_hospitalization_data import FirstPneumoniaHospitalizationData
+from .all_hospitalization_data import AllHospitalizationData
 
 __all__ = [
     "FirstHfHospitalizationData",
     "FirstCopdHospitalizationData",
     "FirstMiHospitalizationData",
     "FirstPneumoniaHospitalizationData",
+    "AllHospitalizationData"
 ]
