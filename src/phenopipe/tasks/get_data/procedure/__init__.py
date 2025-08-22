e830b6e7911b43d83dc9c4b5385374d849ad149d
cakarac <adnancihan.cakar@vumc.org>
2025-08-22 20:25:51 +0000

    adds survey queries

--- src/phenopipe/tasks/get_data/procedure/__init__.py
+++ src/phenopipe/tasks/get_data/procedure/__init__.py
@@ -2,11 +2,11 @@ from .first_stent_data import FirstStentData
 from .first_ca_bypass_graft_data import FirstCaBypassGraftData
 from .first_total_hip_arthoplasty_data import FirstTotalHipArthoplastyData
 from .first_total_knee_arthoplasty_data import FirstTotalKneeArthoplastyData
-from .all_pci_data  import AllPciData
-from .all_thrombolysis_data  import AllThrombolysisData
+from .all_pci_data import AllPciData
+from .all_thrombolysis_data import AllThrombolysisData
 from .first_total_knee_arthoplasty_data import FirstTotalKneeArthoplastyData
 from .first_cpap_data import FirstCpapData
-from .all_cabg_data  import AllCabgData
+from .all_cabg_data import AllCabgData
 from .first_bariatric_surgery_data import FirstBariatricSurgeryData
 from .first_cabg_data import FirstCabgData
 
@@ -15,13 +15,10 @@ __all__ = [
     "FirstCaBypassGraftData",
     "FirstTotalHipArthoplastyData",
     "FirstTotalKneeArthoplastyData",
-    "AllPciData", 
-    "AllThrombolysisData", 
-    "FirstCpapData", 
-    "AllCabgData", 
-    "FirstBariatricSurgeryData", 
-    "FirstCabgData"
+    "AllPciData",
+    "AllThrombolysisData",
+    "FirstCpapData",
+    "AllCabgData",
+    "FirstBariatricSurgeryData",
+    "FirstCabgData",
 ]
-
-
-
