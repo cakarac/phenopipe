513164e3baa595d5d3d702cd807e42be4de2ef36
cakarac <adnancihan.cakar@vumc.org>
2025-08-22 19:20:52 +0000

    adds beta blockers query

--- src/phenopipe/tasks/get_data/medication/__init__.py
+++ src/phenopipe/tasks/get_data/medication/__init__.py
@@ -3,6 +3,7 @@ from .first_ace_inhibitors_data import FirstAceInhibitorsData
 from .first_arb_meds_data import FirstArbMedsData
 from .first_anti_hypertensives_data import FirstAntiHypertensivesData
 from .first_aspirin_data import FirstAspirinData
+from .first_beta_blockers_data import FirstBetaBlockersData
 
 __all__ = [
     "FirstStatinsData",
@@ -10,4 +11,5 @@ __all__ = [
     "FirstArbMedsData",
     "FirstAntiHypertensivesData",
     "FirstAspirinData",
+    "FirstBetaBlockersData"
 ]
--- src/phenopipe/vocab/terms/medications/__init__.py
+++ src/phenopipe/vocab/terms/medications/__init__.py
@@ -3,11 +3,13 @@ from .ace_inhibitors_vocab import ACE_INHIBITORS_TERMS
 from .arb_meds_vocab import ARB_MEDS_TERMS
 from .anti_hypertensives_vocab import ANTI_HYPERTENSIVES_TERMS
 from .aspirin_vocab import ASPIRIN_TERMS
+from .beta_blockers_vocab import BETA_BLOCKERS_TERMS
 
 __all__ = [
     "STATINS_TERMS",
     "ACE_INHIBITORS_TERMS",
     "ARB_MEDS_TERMS",
     "ANTI_HYPERTENSIVES_TERMS",
-    "ASPIRIN_TERMS"
+    "ASPIRIN_TERMS",
+    "BETA_BLOCKERS_TERMS"
 ]
