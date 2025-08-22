e43295ad80b16c2b8ed91298d618558f44a340fa
cakarac <adnancihan.cakar@vumc.org>
2025-08-22 19:18:37 +0000

    adds aspirin query

--- src/phenopipe/tasks/get_data/medication/__init__.py
+++ src/phenopipe/tasks/get_data/medication/__init__.py
@@ -2,10 +2,12 @@ from .first_statins_data import FirstStatinsData
 from .first_ace_inhibitors_data import FirstAceInhibitorsData
 from .first_arb_meds_data import FirstArbMedsData
 from .first_anti_hypertensives_data import FirstAntiHypertensivesData
+from .first_aspirin_data import FirstAspirinData
 
 __all__ = [
     "FirstStatinsData",
     "FirstAceInhibitorsData",
     "FirstArbMedsData",
     "FirstAntiHypertensivesData",
+    "FirstAspirinData",
 ]
--- src/phenopipe/vocab/terms/medications/__init__.py
+++ src/phenopipe/vocab/terms/medications/__init__.py
@@ -2,10 +2,12 @@ from .statins_vocab import STATINS_TERMS
 from .ace_inhibitors_vocab import ACE_INHIBITORS_TERMS
 from .arb_meds_vocab import ARB_MEDS_TERMS
 from .anti_hypertensives_vocab import ANTI_HYPERTENSIVES_TERMS
+from .aspirin_vocab import ASPIRIN_TERMS
 
 __all__ = [
     "STATINS_TERMS",
     "ACE_INHIBITORS_TERMS",
     "ARB_MEDS_TERMS",
     "ANTI_HYPERTENSIVES_TERMS",
+    "ASPIRIN_TERMS"
 ]
