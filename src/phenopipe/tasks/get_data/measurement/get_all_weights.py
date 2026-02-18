be7c01e667d483e3677d556499205106936c01f3
cakarac <adnancihan.cakar@vumc.org>
2026-02-18 21:51:34 +0000

    updates weight and height query names

--- src/phenopipe/tasks/get_data/measurement/get_all_weights.py
+++ src/phenopipe/tasks/get_data/measurement/get_all_weights.py
@@ -2,5 +2,5 @@ from phenopipe.tasks.get_data.fixed_query import FixedQuery
 from phenopipe.query_builders.fixed_queries import WEIGHT_QUERY
 
 
-class GetWeight(FixedQuery):
+class GetAllWeights(FixedQuery):
     query: str = WEIGHT_QUERY
