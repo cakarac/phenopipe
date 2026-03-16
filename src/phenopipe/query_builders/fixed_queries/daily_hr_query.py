7ef7546fc8adff5eb23005ca7bf3976c82cd7b52
cakarac <adnancihan.cakar@vumc.org>
2026-03-16 18:14:45 +0000

    updates daily heart rate query

--- src/phenopipe/query_builders/fixed_queries/daily_hr_query.py
+++ src/phenopipe/query_builders/fixed_queries/daily_hr_query.py
@@ -1,6 +1,8 @@
 DAILY_HR_QUERY = """
 SELECT person_id, 
                     CAST(datetime AS DATE) AS date,
+                    MIN(heart_rate_value) AS min_daily_heart_rate,
+                    MAX(heart_rate_value) AS max_daily_heart_rate,
                     AVG(heart_rate_value) AS mean_daily_heart_rate, 
                     STDDEV(heart_rate_value) AS sd_daily_heart_rate
             FROM heart_rate_minute_level 
